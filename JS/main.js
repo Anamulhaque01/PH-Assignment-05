55874cbe1acb71fa594b67123ed94de79a4a8c0c
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-03-08 10:32:59 +0600

    Labels added to the cards

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,8 @@
 const mainContainer = document.getElementById("main-container")
+// const countMainContainer = container.children.length;
+
+// console.log(countMainContainer)
+
 
 
 
@@ -19,6 +23,21 @@ function displayAllElement(issues) {
         let priorityColor = "";
         let BorderColor = "";
         let statusImg = "";
+        let hasBug = "";
+
+        if (issue.labels.includes("bug") === true) {
+            hasBug = `<div class="rounded-full px-3 py-1 bg-red-200 text-red-500 text-[15px] flex items-center gap-1"><img src="assets/Vector (1).png" alt=""> BUG</div>
+
+                            <div class="rounded-full px-3 py-1 bg-orange-200 text-orange-500 text-[15px] flex items-center gap-1"><img src="assets/Vector.png" alt=""> HELP WANTED</div>`
+        }
+
+        else if (issue.labels.includes("enhancement") === true) {
+            hasBug = `<div class="rounded-full px-3 py-1 bg-green-200 text-green-500 text-[15px] flex items-center gap-1"><img src="assets/Vector-3.png" alt=""> ENHANCEMENT</div>`
+        }
+
+        else {
+            hasBug = `<div class="rounded-full px-3 py-1 bg-blue-200 text-blue-500 text-[15px] flex items-center gap-1"><i class="fa-regular fa-clipboard"></i> DOCUMENTED</div>`
+        }
 
         if (issue.status.toLowerCase() === "open") {
             borderColor = "border-green-600";
@@ -39,7 +58,7 @@ function displayAllElement(issues) {
 
         console.log(issue);
         const card = document.createElement("div");
-        card.className = `shadow bg-white rounded border-t-6 ${borderColor}`;
+        card.className = `shadow bg-white rounded border-t-6 ${borderColor} hover:cursor-pointer`;
         card.innerHTML = `<div class="flex items-center justify-between p-5">
                         <img src="${statusImg}" alt="">
                         <div class="rounded-full px-7 py-1 ${priorityColor}  text-[15px]" id = "priority">${issue.priority.toUpperCase()}</div>
@@ -50,9 +69,7 @@ function displayAllElement(issues) {
                         <p class="line-clamp-2 text-[#64748B] mb-4">${issue.description}</p>
 
                         <div class="flex items-center gap-2 pb-5">
-                            <div class="rounded-full px-3 py-1 bg-red-200 text-red-500 text-[15px] flex items-center gap-1"><img src="assets/Vector (1).png" alt=""> BUG</div>
-
-                            <div class="rounded-full px-3 py-1 bg-orange-200 text-orange-500 text-[15px] flex items-center gap-1"><img src="assets/Vector.png" alt=""> HELP WANTED</div>
+                            ${hasBug}
                         </div>
                         </div>
 
