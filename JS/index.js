89ce23b0073e9a492499e5e7e23265b281a1dace
Anamulhaque01 <anamulhaque0357@gmail.com>
2026-03-09 11:08:34 +0600

    Login Funtinality Added

---
+++ JS/index.js
@@ -0,0 +1,23 @@
+document.getElementById("login-btn").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const userName = document.getElementById("userName");
+    const userNameValue = userName.value;
+
+    const userPass = document.getElementById("userPass");
+    const userPassValue = userPass.value;
+
+    if (userNameValue === "admin" && userPassValue === "admin123") {
+        alert("Login Success");
+        window.location.assign("/main.html");
+        userName.value = "";
+        userPass.value = "";
+    }
+    else {
+        alert("Login Failed");
+        userName.value = "";
+        userPass.value = "";
+        return;
+    }
+
+})
